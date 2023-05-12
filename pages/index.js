6f66ff1b4a97bfd2c1656cd356070193eb2d87fc
Rishabh1260 <rishabhjat1260@gmail.com>
2023-05-12 18:37:20 +0530

    changes in pages folder and next.config file

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { Footer, Navbar } from '../components';
+import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+const Page = () => (
+  <div className='bg-primary-black overflow-hidden'>
+    <Navbar />
+    <Hero/>
+    <div className='relative'>
+    <About />
+    <div className='gradient-03 z-0'></div>
+    <Explore />
+    </div>
+    
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
+);
+
+export default Page;
