34f72dfa65d49825cf449f4b3c7785a23ca9745c
Rishabh1260 <rishabhjat1260@gmail.com>
2023-05-12 22:39:08 +0530

    Basic error of  jsx file

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
-    <Hero/>
-    <div className='relative'>
-    <About />
-    <div className='gradient-03 z-0'></div>
-    <Explore />
+    <Hero />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
     </div>
-    
     <GetStarted />
     <WhatsNew />
     <World />
